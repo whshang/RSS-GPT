08317423c94da697b95bbcfc449766607b2be042
Wenhuan Shang <whshang@gmail.com>
2024-06-20 16:16:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -269,16 +269,16 @@ def output(sec, language):
             elif OPENAI_API_KEY:
                 token_length = len(cleaned_article)
                 try:
-                    entry.summary = gpt_summary(cleaned_article,model="gpt-3.5-turbo", language=language)
+                    entry.summary = gpt_summary(cleaned_article,model="random", language=language)
                     with open(log_file, 'a') as f:
                         f.write(f"Token length: {token_length}\n")
                         f.write(f"Summarized using GPT-3.5-turbo\n")
                 except:
                     try:
-                        entry.summary = gpt_summary(cleaned_article,model="gpt-4-turbo-preview", language=language)
+                        entry.summary = gpt_summary(cleaned_article,model="gpt-4o", language=language)
                         with open(log_file, 'a') as f:
                             f.write(f"Token length: {token_length}\n")
-                            f.write(f"Summarized using GPT-4-turbo-preview\n")
+                            f.write(f"Summarized using GPT-4o\n")
                     except Exception as e:
                         entry.summary = None
                         with open(log_file, 'a') as f:
